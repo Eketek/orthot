df3776e96f4f5c5c675d2a60719226e8e9f3cb46
Eketek <>
2019-10-07 13:48:25 -0400

    Background Visual Coherence

--- orthot/libek/hackground.js
+++ orthot/libek/hackground.js
@@ -1,4 +1,7 @@
 export { Hackground }
+import { clamp } from './util.js'
+//  Paints random gradients onto a canvas
+//
 
 let Hackground = function(canvas) {
   if (canvas.jquery) {
@@ -6,10 +9,19 @@ let Hackground = function(canvas) {
   }
   this.resolution = 32
   this.xOFS = 0
-  this.yOFS = 0
+  let _yOFS = 0
+  
+  let minYOFS  = -canvas.height/2
+  let maxYOFS  = canvas.height/2
+  Object.defineProperty(this, 'yOFS', {
+    set: function(val) { 
+      _yOFS = clamp(val, minYOFS, maxYOFS)
+    } 
+  })
   
   let mainCTX = canvas.getContext('2d')
   
+  // Use a buffer twice as 
   let buf = document.createElement("canvas")
   buf.width = canvas.width+this.resolution*2
   buf.height = canvas.height * 2
@@ -20,14 +32,14 @@ let Hackground = function(canvas) {
     [ {
         pos:0,
         color:"black",
-        rndRange:0,
-        rndCurve:1
+        range:0,
+        curve:1
       },
       {
         pos:1,
         color:"black",
-        rndRange:0,
-        rndCurve:1
+        range:0,
+        curve:1
       }
     ]
   ]
@@ -63,7 +75,6 @@ let Hackground = function(canvas) {
         sx = 0
         dx = shiftAMT
       }
-      
         
       //void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
       ctx.drawImage(buf, sx,0,w,buf.height, dx,0,w,buf.height)
@@ -91,29 +102,43 @@ let Hackground = function(canvas) {
     }
     let h = canvas.height
     for (let layer of this.layers) {
+      let layerOFS = ( layer.baseOfs != undefined ? layer.baseOfs : 0 )
+      let mg = layer.baseGen
+      //console.log(layer)
+      let mg_range = layer.range != undefined ? layer.range : 0
+      let mg_curve = layer.curve != undefined ? layer.curve : 1
+      
+      let gradGen = layer.gradGen ? layer.gradGen : Math.random
+      
       for (let col = startCOL; col < endCOL; col++) {
+        let colOFS = layerOFS + (mg ? ((mg(col+this.offset, numCols)**mg_curve)*2*mg_range)-mg_range : 0)
+        //colOFS = 0
+        //baseOFS = 0
+        //console.log(mg)
+//        console.log((mg ? ((mg(col+this.offset, numCols)**mg_curve)*2*mg_range)-mg_range : 0))
         let x1 = col*this.resolution
         let prevPos = 0
         let prevColor = undefined
         let stops = []
         //let grad = ctx.createLinearGradient(0, 0, 150, 150);
-        for (let entry of layer) {
-          let gen = Math.random
-          if (entry.gen) {
-            gen = entry.gen
-          }
-          let pos = (0.5 + entry.pos + (((gen(col+this.offset, numCols) ** entry.rndCurve) * entry.rndRange)*2)-entry.rndRange) * h
+        for (let entry of layer.gradients) {
+          let eg = entry.gen != undefined ? entry.gen : gradGen
+          let eg_range = (entry.range != undefined ? entry.range : 0)
+          let eg_curve = (entry.curve != undefined ? entry.curve : 1)
+          
+          //console.log(entry, eg, eg_range, eg_curve)
+          
+          let pos = (0.5 + colOFS + entry.pos + (((eg(col+this.offset, numCols) ** eg_curve) * eg_range)*2)-eg_range) * h
           let color = entry.color
-          if (pos > prevPos || entry.overdraw) {
+          if (prevColor && (pos > prevPos || entry.overdraw)) {
             let grad = ctx.createLinearGradient(0, prevPos, 0, pos)
             grad.addColorStop(0, prevColor)
             grad.addColorStop(1, entry.color)
             
             ctx.fillStyle = grad
             ctx.fillRect(x1, prevPos, this.resolution, pos-prevPos+1)
-  
-            prevPos = pos
           }          
+          prevPos = pos
           if (entry.startColor) {
             prevColor = entry.startColor
           }
@@ -125,14 +150,7 @@ let Hackground = function(canvas) {
     }
   }
   this.update = function() {
-    if (this.bkgColor) {
-      mainCTX.fillStyle = this.bkgColor
-      mainCTX.fillRect(0,0, canvas.width, canvas.height)
-    }
-    else {
-      mainCTX.clearRect(0,0, canvas.width, canvas.height)      
-    }
-    mainCTX.drawImage(buf, this.xOFS, this.yOFS+canvas.height/2,canvas.width,canvas.height, 0,0,canvas.width,canvas.height)
+    mainCTX.drawImage(buf, this.xOFS, _yOFS+canvas.height/2,canvas.width,canvas.height, 0,0,canvas.width,canvas.height)
   }
 }
 
